28c2bb3b12d3a821f8f8d70fac81141f13067185
zcomert <zcomert@samsun.edu.tr>
2023-11-06 10:30:15 +0300

    SortApp has been added.

---
+++ SortApp/Program.cs
@@ -0,0 +1,20 @@
+﻿var numbers = new List<int>();
+numbers.Add(44);
+numbers.Add(23);
+numbers.Add(55);
+numbers.Add(61);
+numbers.Add(53);
+
+Console.WriteLine($"Size of list :{numbers.Count}");
+foreach (var number in numbers)
+{
+    System.Console.WriteLine(number);
+}
+
+numbers.Sort();
+
+System.Console.WriteLine(new String('-',5));
+foreach (var number in numbers)
+{
+    System.Console.WriteLine(number);
+}
