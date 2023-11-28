b4f924e3f562fba27da33649fb0daada378bafce
zcomert <zcomert@samsun.edu.tr>
2023-11-28 12:01:58 +0300

    Stack has been implemented.

---
+++ Generic/Stack/Stack.cs
@@ -0,0 +1,25 @@
+using Stack.Contracts;
+
+namespace Stack;
+public class Stack<T> : IStack<T>
+{
+    private IStack<T> collection;
+    public Stack()
+    {
+        collection = new LinkedListStack<T>();
+    }
+    public T Peek()
+    {
+        return collection.Peek();
+    }
+
+    public T Pop()
+    {
+        return collection.Pop();
+    }
+
+    public void Push(T item)
+    {
+        collection.Push(item);
+    }
+}
