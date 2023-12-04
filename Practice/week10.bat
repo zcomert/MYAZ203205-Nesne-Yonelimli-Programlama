f773beec728555af3ed56357350474266fc4db88
Furkacan Demircan <furkancan.demircan@gmail.com>
2023-12-04 09:27:47 +0300

    Week9 Practice document added!

---
+++ Practice/week10.bat
@@ -0,0 +1,19 @@
+mkdir Week10
+cd Week10
+mkdir ShoppingPlatform
+cd ShoppingPlatform
+
+dotnet new sln -n ShopPlatform
+dotnet new webapi -o ShopApi
+dotnet new classlib -o ShopEntities
+dotnet new classlib -o ShopRepositories
+
+dotnet sln ShopPlatform.sln add ShopApi
+dotnet sln ShopPlatform.sln add ShopEntities
+dotnet sln ShopPlatform.sln add ShopRepositories
+
+dotnet add ShopApi reference ShopRepositories
+dotnet add ShopApi reference ShopEntities
+dotnet add ShopRepositories reference ShopEntities
+
+code .
