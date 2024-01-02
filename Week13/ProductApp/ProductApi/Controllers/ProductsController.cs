fe6ce31aa9255c5fd914814c51b20977263fe9b2
Furkacan Demircan <furkancan.demircan@gmail.com>
2024-01-02 09:16:16 +0300

    Week 14 bat file added!

--- Week13/ProductApp/ProductApi/Controllers/ProductsController.cs
+++ Week13/ProductApp/ProductApi/Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+using Entities.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Repositories;
@@ -8,20 +9,52 @@ namespace ProductApi.Controllers;
 [Route("api/products")]
 public class ProductsController : ControllerBase
 {
-    private readonly RepositoryContext _context;
+    private readonly ProductRepository repository;
 
-    public ProductsController(RepositoryContext context)
+    public ProductsController(ProductRepository repository)
     {
-        _context = context;
+        this.repository = repository;
+
         // _context = new RepositoryContext(new DbContextOptionsBuilder<RepositoryContext>().Options);
     }
 
     [HttpGet]
     public IActionResult GetAllProducts()
     {
-        var model = _context.Products
-        .Include(p => p.Comments);
-        
-        return Ok(model);
+        return Ok(repository.GetAllProduct());
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetOneProducts(int id)
+    {
+        return Ok(repository.GetOneProduct(id));
+    }
+
+    [HttpPost]
+    public IActionResult CreateProduct(string ProductName, decimal Price)
+    {
+        var prd = new Product(){
+            ProductName = ProductName,
+            Price = Price
+        };
+        repository.CreateProduct(prd);
+        return Created("Product is created!", prd);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateProduct(int id, string ProductName, decimal Price)
+    {
+        var prd = new Product(){
+            ProductName = ProductName,
+            Price = Price
+        };
+        repository.UpdateProduct(id, prd);
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteOneProduct(int id)
+    {
+        return Ok(repository.DeleteOneProduct(id));
     }
 }
--- Week13/ProductApp/ProductApi/Program.cs
+++ Week13/ProductApp/ProductApi/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<ProductRepository>();
+builder.Services.AddScoped<CommentRepository>();
+
 // REGISTER
 builder.Services.AddDbContext<RepositoryContext>(options =>
 {
